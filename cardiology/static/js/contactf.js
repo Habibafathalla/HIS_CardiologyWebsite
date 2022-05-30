4ae888519fa4b2386a792a3792d307ea16020a4f
RawanFekry <93431157+RawanFekry@users.noreply.github.com>
2022-05-30 22:52:11 +0200

    validations edit

--- cardiology/static/js/contactf.js
+++ cardiology/static/js/contactf.js
@@ -1,4 +1,7 @@
+
 const form_1 = document.querySelector('#contact')
+const doctor = document.querySelector('#doctor')
+const Input = document.getElementById("hf");
 const messageInput = document.getElementById("Message");
 
 
@@ -8,11 +11,22 @@ form_1.addEventListener('submit',(event)=>{
       event.preventDefault();
      }
    })
+   
    function validateForm_1(){
-    
+    if(doctor.value=='Select your doctor'){
+      setError(Input,"please select your doctor");
+      return true;
+     
+    }
+    else{
+      clearerror(Input);
+      return false;
+     
+    }
     if(messageInput.value.trim()==''){
         setError(messageInput,"please enter a message");
         return true;
+      
       }
       else{
         clearerror(messageInput);
@@ -32,8 +46,6 @@ form_1.addEventListener('submit',(event)=>{
     const parent=element.parentElement;
     parent.classList.remove('error');
   }
-  
-  
-  
+
   
   
